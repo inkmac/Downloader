a074cfda0887cd536e662b718fcabade1bb30459
inkmas <wenyuan_zhao@outlook.com>
2025-06-22 12:21:08 +0800

    feat(expire): remove software expiration check

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-import sys
-from datetime import datetime
-
 from PySide6.QtWidgets import QApplication
 
-from src.core import expire
 from src.downloader import Downloader
 
 
 def main():
-    expire.initialize_config()
-
-    if expire.is_expired():
-        sys.exit(1)
-
-    if expire.save_current_time(datetime.now()):
-        sys.exit(1)
-
     app = QApplication([])
     window = Downloader()
     window.show()
